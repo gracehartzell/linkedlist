6b5f00b0444da2ceec8638220908baf60d37019c
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-01-10 14:00:33 -0600

    wired up suite properly

---
+++ src/linkedlist.js
@@ -0,0 +1,19 @@
+const Node = require('./node');
+
+class LinkedList { 
+    constructor() 
+    { 
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    } 
+  
+    // add()  
+    // get() 
+    // indexOf()  
+    // insertAt()
+    // removeAt()
+
+} 
+
+module.exports = LinkedList
