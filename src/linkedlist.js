c1b1e6ce4649dd1fb42bd5bae07350b21b5a2901
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-11 10:15:56 -0600

    Add passing

--- src/linkedlist.js
+++ src/linkedlist.js
@@ -1,19 +1,34 @@
-const Node = require('./node');
+const Node = require("./node");
 
-class LinkedList { 
-    constructor() 
-    { 
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
-    } 
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(data) {
+    const node = new Node(data);
+    if (this.length === 0) this.head = node;
+    else {
+      let newNode = this.head;
+      let notNew;
+      while (newNode.next) {
+        notNew = newNode;
+        newNode = newNode.next;
+      }
+      node.prev = newNode;
+      newNode.next = node;
+    }
+    this.tail = node;
+    this.length++;
+  }
+
+  // get()
+  indexOf()
   
-    // add()  
-    // get() 
-    // indexOf()  
-    // insertAt()
-    // removeAt()
+  // insertAt()
+  // removeAt()
+}
 
-} 
-
-module.exports = LinkedList
+module.exports = LinkedList;
